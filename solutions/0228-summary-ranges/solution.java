2a6bc9938a40da924188fa4d1a28f7aa3318f2e7
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2023-06-12 04:55:29 +0000

    [Leetcode Solutions Sync] Runtime - 5 ms (75.69%), Memory - 41.1 MB (98.55%)

---
+++ solutions/0228-summary-ranges/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<String>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            int temp = nums[i];
+            while(i + 1 < nums.length && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+            if(temp != nums[i])
+                result.add(temp + "->" + nums[i]);
+            else
+                result.add(String.valueOf(temp)); // String.valueOf(temp) is much better than Integer.toString(temp) (+40% ???)
+        }
+        return result;
+    }
+}
