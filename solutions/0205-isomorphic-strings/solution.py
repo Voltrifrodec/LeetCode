72549e76874e0d87f8039cb4fb48358081e28f07
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2024-04-02 21:18:27 +0000

    [Leetcode Solutions Sync] Runtime - 48 ms (5.21%), Memory - 17.2 MB (14.17%)

---
+++ solutions/0205-isomorphic-strings/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        
+        str1 = []
+        str1.extend(s.index(i) for i in s)
+        str2 = []
+        str2.extend(t.index(i) for i in t)
+                
+        return True if str1 == str2 else False
+        
