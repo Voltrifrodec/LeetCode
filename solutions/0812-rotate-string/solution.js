01867fa882660e063eda286841bc0876ded5607c
James V. Frodec <69749982+Voltrifrodec@users.noreply.github.com>
2024-11-03 22:41:10 +0000

    [Leetcode Solutions Sync] Runtime - 0 ms (100.00%), Memory - 48.8 MB (61.89%)

--- solutions/0812-rotate-string/solution.js
+++ solutions/0812-rotate-string/solution.js
@@ -1,19 +1,9 @@
+// Riesenie 2: Po prechadzani rieseni ma zaujala moznost hladat goal v zlozenom retazci s+s. [O(n), T(n)]
 /**
  * @param {string} s
  * @param {string} goal
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
-	if (s == goal)
-		return true;
-
-    for (let i = 0; i < s.length; i++) {
-		let first_c = s[0];
-		s = s.slice(1, s.length) + first_c;
-
-		if (s == goal)
-			return true;
-	}
-	
-	return false;
+	return s.length == goal.length && (s+s).includes(goal);
 };
